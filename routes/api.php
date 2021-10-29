78cf31471e5bd4f5bb1215920e840bceb2915875
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-10-29 17:50:29 +0700

    API Webhook + Helper + Fix

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\WebhookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,5 @@ use App\Http\Controllers\OrderController;
 
 Route::post('orders', [OrderController::class, 'create']);
 Route::get('orders', [OrderController::class, 'index']);
+
+Route::post('webhook', [WebhookController::class, 'midtransHandler']);
